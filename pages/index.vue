5d562c7d5664e25e6b07cdac4b5a173d7b93826d
Andres Cruz <andrscyv@gmail.com>
2021-01-11 14:49:25 -0600

    add snake alignment to divs

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        los_dominos
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <div>
+    Los dominos
+    <div class="game">
+      <div
+        v-for="(row,i) in gameRows"
+        :key="i"
+        :style="{ flexDirection: i % 2 == 0 ? 'row' : 'row-reverse'}"
+        class="game-row"
+      >
+        <div
+          v-for="(tile,idx) in row"
+          :key="idx"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+          {{ tile }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+    //   tilesPlayed: [
+    //     [1, 2],
+    //     [1, 2],
+    //     [1, 2],
+    //     [1, 2],
+    //     [1, 2],
+    //     [1, 2],
+    //     [1, 2]
+    //   ]
+      tilesPerRow: 10
+    }
+  },
+  computed: {
+    tilesPlayed () {
+      const tiles = []
+      for (let index = 0; index < 28; index++) {
+        tiles.push([index, 1])
+      }
+      return tiles
+    },
+    gameRows () {
+      const rows = []
+      let currentRow = []
+
+      this.tilesPlayed.forEach((tile) => {
+        if (currentRow.length === this.tilesPerRow) {
+          rows.push(currentRow)
+          currentRow = []
+        }
+        currentRow.push(tile)
+      })
+
+      if (rows.flat().length < this.tilesPlayed.length) { rows.push(currentRow) }
+
+      return rows
+    }
+  }
+
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+.game-row{
+    display: flex;
 }
 </style>
